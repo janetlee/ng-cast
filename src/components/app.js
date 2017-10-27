8f3f38270f89fb0523065340a030a592424800a7
Hack Reactor Student <student@hackreactor.com>
2017-10-27 15:44:46 -0700

    Update to render UI correctly with fake data

--- src/components/app.js
+++ src/components/app.js
@@ -2,4 +2,9 @@ angular.module('video-player')
 
 .component('app', {
   // TODO
+  controller: function() {
+    this.videos = window.exampleVideoData;
+    console.log('in App', this.videos);
+  },
+  templateUrl: '/src/templates/app.html'
 });
--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,4 +1,12 @@
 angular.module('video-player')
 .component('videoList', {
   // TODO
+  bindings: {
+    videos: '<',
+    test: '@'
+  },
+  controller: function() {
+    // debugger;
+  },
+  templateUrl: '/src/templates/videoList.html'
 });
